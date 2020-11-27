af4cd2ff026aff4e5b8f02b5dadc937807784f1c
Korisnik <tanja_stanic@uns.ac.rs>
2020-11-27 23:30:41 +0100

    Communication tests

---
+++ kp/bank-acquirer/src/main/java/com/example/bankacquirer/controller/Test.java
@@ -0,0 +1,31 @@
+package com.example.bankacquirer.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class Test {
+	
+	//premjestiti u config class kasnije
+	@Bean
+	public RestTemplate restTemplate() {
+	    return new RestTemplate();
+	}
+	
+	@Autowired
+	private RestTemplate restTemplate;
+	
+	@GetMapping("/testBankAcquirer")
+	public String testCommunication() {
+		restTemplate = new RestTemplate();
+        String fromBankAq = restTemplate.getForObject("http://localhost:8441/testBankMS", String.class);
+		System.out.println(fromBankAq);
+		fromBankAq+=", Bank Acquirer Test-done";
+		return fromBankAq;
+	}
+	
+}
