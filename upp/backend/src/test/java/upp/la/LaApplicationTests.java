ddb126c1bc6adecf4b006d29d88fd5928418b04b
Aleksandar <saksofon997@gmail.com>
2020-12-09 16:00:04 +0100

    Initialize UPP backend

---
+++ upp/backend/src/test/java/upp/la/LaApplicationTests.java
@@ -0,0 +1,13 @@
+package upp.la;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
