fb0b50914674a36b652a2375d04de799f5d49890
Aleksandar Petaković <saksofon997@gmail.com>
2020-12-12 16:05:00 +0100

    Tanja's PR (#6)

---
+++ upp/backend/src/main/java/upp/la/service/RegistrationService.java
@@ -0,0 +1,58 @@
+package upp.la.services;
+
+import org.camunda.bpm.engine.IdentityService;
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.camunda.bpm.engine.identity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import upp.la.dto.FormFieldDto;
+
+import java.util.List;
+
+@Service
+public class RegistrationService implements JavaDelegate {
+
+    @Autowired
+    IdentityService identityService;
+
+    @Override
+    public void execute(DelegateExecution execution) throws Exception {
+
+        List<FormFieldDto> registration =
+            (List<FormFieldDto>) execution.getVariable("registration");
+        User user = identityService.newUser("");
+        upp.la.model.User userModel = new upp.la.model.User();
+
+        for (FormFieldDto formField : registration) {
+
+            if (formField.getFieldId().equals("firstNameId")) {
+                user.setFirstName(formField.getFieldValue());
+                userModel.setFirstName(formField.getFieldValue());
+            }
+            if (formField.getFieldId().equals("lastNameId")) {
+                user.setLastName(formField.getFieldValue());
+                userModel.setLastName(formField.getFieldValue());
+            }
+            if (formField.getFieldId().equals("emailId")) {
+                user.setEmail(formField.getFieldValue());
+                userModel.setEmail(formField.getFieldValue());
+            }
+            if (formField.getFieldId().equals("passwordId")) {
+                //izvrsiti hesovanje i ostalo
+                user.setPassword(formField.getFieldValue());
+                userModel.setPassword(formField.getFieldValue());
+            }
+            if (formField.getFieldId().equals("cityId")) {
+                userModel.setCity(formField.getFieldValue());
+            }
+            if (formField.getFieldId().equals("stateId")) {
+                userModel.setState(formField.getFieldValue());
+            }
+
+        }
+
+        identityService.saveUser(user);
+        //napraviti repository za cuvanje usera u bazu
+    }
+}
