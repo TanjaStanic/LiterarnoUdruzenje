1440f5cb75ab4c4f86430b4007943a946540b703
Sara Celik <sara.celik@live.com>
2020-12-20 15:30:30 +0100

    pom.xml

--- kp/payment-info/src/main/java/com/example/paymentinfo/PaymentInfoApplication.java
+++ kp/payment-info/src/main/java/com/example/paymentinfo/PaymentInfoApplication.java
@@ -3,13 +3,22 @@ package com.example.paymentinfo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class PaymentInfoApplication {
+public class PaymentInfoApplication implements WebMvcConfigurer {
 
     public static void main(String[] args) {
         SpringApplication.run(PaymentInfoApplication.class, args);
     }
+    
+    @Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry
+				.addResourceHandler("/webjars/**")
+				.addResourceLocations("classpath:/META-INF/resources/webjars/").resourceChain(false);
+	}
 
 }
--- kp/payment-info/src/main/java/com/example/paymentinfo/controller/ViewController.java
+++ kp/payment-info/src/main/java/com/example/paymentinfo/controller/ViewController.java
@@ -2,17 +2,19 @@ package com.example.paymentinfo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
 
 @Controller
 @RequestMapping("view")
 public class ViewController {
 	
-	  @GetMapping("/payment-methods")
+	 @GetMapping("/payment-methods")
 	    public String getPaymentMethods(Model model) {
 	        return "paymentMethods";
-	    }
+	 }
 
 
 }
