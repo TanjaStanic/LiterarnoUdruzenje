6a4ecc4d02716f00b237dfad99d0519b65ab7f4c
tanja_stanic@uns.ac.rs <tanja_stanic@uns.ac.rs>
2021-01-04 12:21:50 +0100

    Send and confirm mail, verify account - done

---
+++ upp/backend/src/main/java/upp/la/service/VerifyAccountService.java
@@ -0,0 +1,47 @@
+package upp.la.service;
+
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import upp.la.error.ErrorMessages;
+import upp.la.exceptions.EntityNotFound;
+import upp.la.model.ConfirmationToken;
+import upp.la.model.User;
+import upp.la.repository.ConfirmationTokenRepository;
+import upp.la.repository.UserRepository;
+
+public class VerifyAccountService implements JavaDelegate{
+
+	
+	@Autowired 
+	ConfirmationTokenRepository confirmationTokenRepository;
+	
+	@Autowired 
+	UserRepository userRepository;
+	
+	@Override
+	public void execute(DelegateExecution execution) throws Exception {
+		
+		String confirmationToken = execution.getVariable("validation_token").toString();
+		ConfirmationToken token =
+		        confirmationTokenRepository.findByConfirmationToken(confirmationToken);
+
+		    if (token == null) {
+		      throw new EntityNotFound(ErrorMessages.TOKEN_ERROR());
+		    }
+
+		    User user = userRepository.findByEmail(token.getUser().getEmail());
+
+		    if (user == null) {
+		      throw new EntityNotFound(ErrorMessages.ENTITY_NOT_FOUND());
+		    }
+
+		    user.setActivated(true);
+
+		    userRepository.save(user);
+		    confirmationTokenRepository.delete(token);
+		
+	}
+
+}
