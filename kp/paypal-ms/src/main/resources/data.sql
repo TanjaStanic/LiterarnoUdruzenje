930e3f84da9b037a44a8a98c5f32c9f6d429cc67
Nataša L <natasalukic2@gmail.com>
2021-02-13 16:10:11 +0100

    Update data.sql

--- kp/paypal-ms/src/main/resources/data.sql
+++ kp/paypal-ms/src/main/resources/data.sql
@@ -3,13 +3,6 @@ insert into paypal_db.currency (`code`, `name`)  values ('EUR', 'Euro');
 insert into paypal_db.currency (`code`, `name`)  values ('BAM', 'Convertible mark');
 insert into paypal_db.currency (`code`, `name`)  values ('USD', 'United States dollar');
 
--- Literarno udruzenje
-insert into paypal_db.client (`client_id`, `client_secret`, `email`)
- values ('fYa7wChWdp7cFDBevRLpl8wfN6LXmn6XQCA0RQohAnUHhInkE0VqS7qMCuEbVRQwIJ60r3PgnwiSQSxnDmUTU6rsIS1+LosKbaGcGsXXiJW6jruJiRgld806foS1nnrr', 'C+Gb1rOyrUYi8kW+ryDpsTVZZJBN31uKYIHMVhHAv0WoXuLPxc8oqX4+Mi2uOA3xsPfjuZwpkiF8IfQblQLle4enAiU+1Krs77/hV1uxu/AMbixlOONk41SEeNZwCoy2', 'sb-zx3ys4123984@business.example.com');
-
-insert into crypto (iv, text) values ("utnsQAGFOzUdQ3eE", "fYa7wChWdp7cFDBevRLpl8wfN6LXmn6XQCA0RQohAnUHhInkE0VqS7qMCuEbVRQwIJ60r3PgnwiSQSxnDmUTU6rsIS1+LosKbaGcGsXXiJW6jruJiRgld806foS1nnrr");
-insert into crypto (iv, text) values ("FSgzSBHGBud5OIBS", "C+Gb1rOyrUYi8kW+ryDpsTVZZJBN31uKYIHMVhHAv0WoXuLPxc8oqX4+Mi2uOA3xsPfjuZwpkiF8IfQblQLle4enAiU+1Krs77/hV1uxu/AMbixlOONk41SEeNZwCoy2");
-
 insert into subscription_type (name ) values ("FIXED");
 insert into subscription_type (name ) values ("INFINITE");
 
